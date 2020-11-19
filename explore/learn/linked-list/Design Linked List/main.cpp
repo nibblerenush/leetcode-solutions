79d0416009972b7a405b3f349a54809250d44aa4
Kyamran Shakhaev <shkyga@yandex.ru>
2020-11-19 16:16:38 +0300

    Deleted: repeating from problem "Design Linked List"

--- explore/learn/linked-list/Design Linked List/main.cpp
+++ explore/learn/linked-list/Design Linked List/main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 
 class MyLinkedList {
@@ -7,14 +8,7 @@ private:
     Node* next = nullptr;
   };
 
-  Node* m_head;
-  Node* m_tail;
-  int m_size;
-
 public:
-  MyLinkedList() : m_head(nullptr), m_tail(nullptr), m_size(0)
-  {}
-
   ~MyLinkedList() {
     Node* cur_node = m_head;
 
@@ -24,29 +18,18 @@ public:
       delete prev_node;
     }
   }
-
+  
   int get(int index) {
-    if (index < 0) {
+    if (!checkCorrectIndex(index)) {
       return -1;
     }
-    
-    Node* cur_node = m_head;
-    int cur_index = -1;
-    int result = -1;
-    
-    while (cur_node && cur_index != index) {
-      ++cur_index;
-      result = cur_node->val;
-      cur_node = cur_node->next;
-    }
-
-    return cur_index == index ? result : -1;
+    return getNodeByIndex(index)->val;
   }
   
   void addAtHead(int val) {
     Node* new_node = new Node{ val, m_head };
     m_head = new_node;
-
+    
     if (!m_tail) {
       m_tail = m_head;
     }
@@ -78,75 +61,104 @@ public:
       addAtTail(val);
     }
     else {
-      Node* cur_node = m_head;
-      Node* prev_node = nullptr;
-      int cur_index = -1;
-
-      while (cur_index != index - 1) {
-        ++cur_index;
-        prev_node = cur_node;
-        cur_node = cur_node->next;
-      }
-
-      Node* new_node = new Node{ val, cur_node };
+      auto prev_node = getNodeByIndex(index - 1);
+      Node* new_node = new Node{ val, prev_node->next };
       prev_node->next = new_node;
-
       ++m_size;
     }
   }
 
   void deleteAtIndex(int index) {
-    if (m_size == 0 || index < 0 || index >= m_size) {
+    if (!checkCorrectIndex(index)) {
       return;
     }
-
+    
     if (index == 0) {
       Node* old_head = m_head;
       m_head = m_head->next;
       delete old_head;
-      --m_size;
+
+      if (!m_head) {
+        m_tail = nullptr;
+      }
     }
     else {
-      Node* cur_node = m_head;
-      Node* prev_node = nullptr;
-      int cur_index = -1;
-
-      while (cur_index != index - 1) {
-        ++cur_index;
-        prev_node = cur_node;
-        cur_node = cur_node->next;
-      }
-
+      auto prev_node = getNodeByIndex(index - 1);
+      Node* cur_node = prev_node->next;
       prev_node->next = cur_node->next;
       delete cur_node;
-
+      
       if (index == m_size - 1) {
         m_tail = prev_node;
       }
-
-      --m_size;
     }
+
+    --m_size;
   }
 
   int getSize() {
     return m_size;
   }
+
+private:
+  Node* getNodeByIndex(int index) const {
+    Node* cur_node = m_head;
+    Node* prev_node = nullptr;
+    int cur_index = -1;
+    
+    while (cur_index != index) {
+      ++cur_index;
+      prev_node = cur_node;
+      cur_node = cur_node->next;
+    }
+
+    return prev_node;
+  }
+  
+  bool checkCorrectIndex(int index) const {
+    return m_size != 0 && index >= 0 && index < m_size;
+  }
+
+private:
+  Node* m_head = nullptr;
+  Node* m_tail = nullptr;
+  int m_size = 0;
 };
 
 int main() {
   MyLinkedList linked_list;
-  linked_list.addAtHead(1);
+  
   linked_list.addAtHead(2);
+  linked_list.addAtHead(1);
   linked_list.addAtTail(3);
   linked_list.addAtTail(4);
+
+  assert(linked_list.getSize() == 4);
+  assert(linked_list.get(0) == 1);
+  assert(linked_list.get(1) == 2);
+  assert(linked_list.get(2) == 3);
+  assert(linked_list.get(3) == 4);
+
+  linked_list.deleteAtIndex(0);
+  linked_list.deleteAtIndex(0);
+  linked_list.deleteAtIndex(0);
+  linked_list.deleteAtIndex(0);
+
+  assert(linked_list.getSize() == 0);
+  assert(linked_list.get(0) == -1);
 
   linked_list.addAtIndex(0, 5);
   linked_list.addAtIndex(1, 6);
   linked_list.addAtIndex(1, 7);
   linked_list.addAtIndex(7, 8);
-
   linked_list.deleteAtIndex(7);
   linked_list.addAtTail(9);
+
+  assert(linked_list.getSize() == 4);
+  assert(linked_list.get(0) == 5);
+  assert(linked_list.get(1) == 7);
+  assert(linked_list.get(2) == 6);
+  assert(linked_list.get(3) == 9);
   
   for (int i = 0; i < linked_list.getSize(); ++i) {
     std::cout << i << ' ' << linked_list.get(i) << std::endl;
