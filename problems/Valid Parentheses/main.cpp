e5bf85df096732761de76dc1d13477c787d98920
Kyamran Shakhaev <shkyga@yandex.ru>
2021-01-13 11:04:51 +0300

    Added: problem "Valid Parentheses"

--- problems/Valid Parentheses/main.cpp
+++ problems/Valid Parentheses/main.cpp
@@ -1,11 +1,23 @@
+#include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
 #include <stack>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
-  bool isValid(std::string s) {
-    std::stack<char> work_stack;
+  bool isValid(string s) {
+    stack<char> work_stack;
     for (char c : s) {
       if (c == '(' || c == '[' || c == '{') {
         work_stack.push(c);
@@ -17,7 +29,7 @@ public:
         else {
           char top = work_stack.top();
           work_stack.pop();
-
+          
           if ((top == '(' && c != ')') || (top == '[' && c != ']') || (top == '{' && c != '}')) {
             return false;
           }
@@ -30,10 +42,5 @@ public:
 };
 
 int main() {
-  std::string s;
-  std::cin >> s;
-
-  Solution solution;
-  std::cout << std::boolalpha << solution.isValid(s) << std::endl;
   return 0;
 }
