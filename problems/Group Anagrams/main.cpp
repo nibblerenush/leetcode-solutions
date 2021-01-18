92d10b0259c8dcdc6099eac6d733f37d6cd2d98d
Kyamran Shakhaev <shkyga@yandex.ru>
2021-01-18 11:00:28 +0300

    Added: problem "Group Anagrams"

--- problems/Group Anagrams/main.cpp
+++ problems/Group Anagrams/main.cpp
@@ -1,9 +1,12 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <limits>
 #include <map>
 #include <numeric>
+#include <queue>
 #include <set>
+#include <stack>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
@@ -15,40 +18,20 @@ class Solution {
 public:
   vector<vector<string>> groupAnagrams(vector<string>& strs) {
     unordered_map<string, vector<string>> hashmap;
-    for (const auto& str : strs) {
-      auto sortedStr = str;
-      sort(sortedStr.begin(), sortedStr.end());
-      hashmap[sortedStr].push_back(str);
+    for (const string& str : strs) {
+      string sorted_str = str;
+      sort(sorted_str.begin(), sorted_str.end());
+      hashmap[move(sorted_str)].push_back(str);
     }
-
+    
     vector<vector<string>> result;
     for_each(hashmap.begin(), hashmap.end(), [&result](const auto& item) {
-      result.push_back(item.second);
+      result.push_back(move(item.second));
     });
-
     return result;
   }
 };
 
 int main() {
-  /*int n;
-  cin >> n;
-
-  vector<string> strs(n);
-  for (auto& str : strs) {
-    cin >> str;
-  }*/
-
-  vector<string> strs = {
-    "eat", "tea", "tan", "ate", "nat", "bat"
-  };
-
-  Solution solution;
-  for (const auto& row : solution.groupAnagrams(strs)) {
-    for (const auto& str : row) {
-      cout << str << ' ';
-    }
-    cout << '\n';
-  }
   return 0;
 }
