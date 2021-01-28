ee9b06e5cb909214afecb223bcbcea5e00ab14ce
Kyamran Shakhaev <shkyga@yandex.ru>
2021-01-28 23:11:13 +0300

    Added: problem "Perfect Squares"

--- problems/Perfect Squares/main.cpp
+++ problems/Perfect Squares/main.cpp
@@ -1,16 +1,28 @@
+#include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
 #include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
+
+using namespace std;
 
 class Solution {
 public:
   int numSquares(int n) {
     int result = 0;
     
-    std::queue<std::pair<int, int>> work_queue;
+    queue<pair<int, int>> work_queue;
     work_queue.push({ n, 0 });
     
-    std::vector<bool> visited(n + 1, false);
+    vector<bool> visited(n + 1, false);
     visited[n] = true;
     
     while (!work_queue.empty()) {
@@ -25,7 +37,7 @@ public:
       
       for (int i = 1; i * i <= remain; ++i) {
         int new_remain = remain - i * i;
-
+        
         if (!visited[new_remain]) {
           work_queue.push({ new_remain, step + 1 });
           visited[new_remain] = true;
@@ -38,10 +50,5 @@ public:
 };
 
 int main() {
-  int n = 0;
-  std::cin >> n;
-
-  Solution solution;
-  std::cout << solution.numSquares(n) << std::endl;
   return 0;
 }
