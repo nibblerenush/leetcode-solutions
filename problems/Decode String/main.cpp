851ece3e13c7d70cae98bc2f4dba77e219b33f37
Kyamran Shakhaev <shkyga@yandex.ru>
2021-02-02 14:06:19 +0300

    Added: problem "Decode String"

--- problems/Decode String/main.cpp
+++ problems/Decode String/main.cpp
@@ -1,57 +1,66 @@
+#include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
 #include <stack>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
+
+using namespace std;
 
 class Solution {
 public:
-  std::string decodeString(std::string s) {
-    std::stack<char> work_stack;
+  string decodeString(string s) {
+    // init
+    stack<char> work_stack;
+    
     for (char c : s) {
       if (c != ']') {
         work_stack.push(c);
       }
       else {
-        std::string repeat_string;
+        string repeat_string;
         while (work_stack.top() != '[') {
           repeat_string.push_back(work_stack.top());
           work_stack.pop();
         }
         
         work_stack.pop();
-        std::reverse(repeat_string.begin(), repeat_string.end());
+        reverse(repeat_string.begin(), repeat_string.end());
         
-        std::string repeat_number;
-        while (!work_stack.empty() && std::isdigit(work_stack.top())) {
+        string repeat_number;
+        while (!work_stack.empty() && isdigit(work_stack.top())) {
           repeat_number.push_back(work_stack.top());
           work_stack.pop();
         }
-
-        std::reverse(repeat_number.begin(), repeat_number.end());
-
-        for (int i = 0; i < std::stoi(repeat_number); ++i) {
+        
+        reverse(repeat_number.begin(), repeat_number.end());
+        
+        for (int i = 0; i < stoi(repeat_number); ++i) {
           for (char rc : repeat_string) {
             work_stack.push(rc);
           }
         }
       }
     }
-
-    std::string result;
+    
+    string result;
     while (!work_stack.empty()) {
       result.push_back(work_stack.top());
       work_stack.pop();
     }
-    std::reverse(result.begin(), result.end());
+    
+    reverse(result.begin(), result.end());
     return result;
   }
 };
 
 int main() {
-  std::string s;
-  std::cin >> s;
-
-  Solution solution;
-  std::cout << solution.decodeString(s) << std::endl;
   return 0;
 }
