27566c944b3feeb73fc263f782c62217ec32abec
Kyamran Shakhaev <shkyga@yandex.ru>
2021-02-08 12:42:33 +0300

    Fixed: problem "Peeking Iterator"

--- problems/Peeking Iterator/main.cpp
+++ problems/Peeking Iterator/main.cpp
@@ -37,7 +37,7 @@ public:
 		m_is_cached = false;
 		m_cached_value = 0;
 	}
-	
+
 	// Returns the next element in the iteration without advancing the iterator.
 	int peek() {
 		if (!m_is_cached) {
@@ -46,7 +46,7 @@ public:
 		}
 		return m_cached_value;
 	}
-	
+
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	int next() {
