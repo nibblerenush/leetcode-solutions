a3c7d6d53a06400c38e3da6971e9b3ea2c98c4ff
Kyamran Shakhaev <shkyga@yandex.ru>
2021-12-21 22:50:57 +0300

    Added: problem "Delete Node in a Linked List"

---
+++ problems/Delete Node in a Linked List/main.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  void deleteNode(ListNode* node) {
+    *node = *node->next;
+  }
+};
+
+int main() {
+  return 0;
+}
